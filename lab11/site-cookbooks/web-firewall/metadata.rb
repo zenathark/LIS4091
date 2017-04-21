ed06891af2b262577229df86812b927bb966baca
Juan Carlos (Zenathark) Galan Hernandez <jcgalanh@gmail.com>
2017-04-20 21:39:28 -0500

    Closes #1

---
+++ lab11/site-cookbooks/web-firewall/metadata.rb
@@ -0,0 +1,7 @@
+name             'site-cookbooks/web-firewall'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures site-cookbooks/web-firewall'
+long_description 'Installs/Configures site-cookbooks/web-firewall'
+version          '0.1.0'
